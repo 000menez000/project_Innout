42c7ffa888c2c8e0634b644a2ba4028c28ff4e89
000menez000 <menezeskauah@gmail.com>
2025-03-22 15:19:11 -0300

    Data e hora em conteudo dinamico

--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,8 @@
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
 
+echo $today;
+
 // Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
