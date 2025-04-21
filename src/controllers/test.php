42685b34a2e5706ccd67242b2fe796e7c5d41f8f
000menez000 <menezeskauah@gmail.com>
2025-04-21 16:33:39 -0300

    cria funcoes que calcula hora  trabalhada e hora de almoco

--- src/controllers/test.php
+++ src/controllers/test.php
@@ -1,17 +1,7 @@
 <?php
 // Controller Temporário
 
-$i1 = DateInterval::createFromDateString('9 hours');
-$i2 = DateInterval::createFromDateString('6 hours');
+loadModel('WorkingHours');
 
-$r1 = sumIntervals($i1, $i2);
-$r2 = subtractIntervals($i1, $i2);
-
-print_r($r1);
-echo '<br>';
-print_r($r2);
-echo '<br>';
-print_r(getDateFromInterval($r2));
-echo '<br>';
-print_r(getDateFromInterval($r2));
-
+$wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
+$wh->getTimes();
